f406961d0a7d60a4685344f1d40c1c057d907388
Christopher Arndt <chris@chrisarndt.de>
2019-12-16 08:46:06 +0100

    Wait for lock before opening third window
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- examples/multiple_windows.py
+++ examples/multiple_windows.py
@@ -1,18 +1,20 @@
-import webview
-import threading
-
 """
 This example demonstrates how to create and manage multiple windows
 """
 
+import webview
 
-def third_window():
+
+def add_third_window(mainwin):
     # Create a new window after the loop started
-    third_window = webview.create_window('Window #3', html='<h1>Third Window</h1>')
+    if not mainwin.shown.wait(10):
+        print("Main window failed to start.")
+    else:
+        third_window = webview.create_window('Window #3', html='<h1>Third Window</h1>')
 
 
 if __name__ == '__main__':
     # Master window
     master_window = webview.create_window('Window #1', html='<h1>First window</h1>')
     second_window = webview.create_window('Window #2', html='<h1>Second window</h1>')
-    webview.start(third_window)
+    webview.start(add_third_window, args=master_window)
